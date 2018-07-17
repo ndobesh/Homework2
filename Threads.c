9e46e27e7182097412e96f0d1df6fbcc099d66f3
ndobesh <31423950+ndobesh@users.noreply.github.com>
2018-07-17 17:59:34 -0500

    Added comments

--- Threads.c
+++ Threads.c
@@ -5,16 +5,20 @@ pthread_t threads[8];
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void *myFunc(void *thread);
-double getPI(int decimals, int precision);
+double getPI();
 
-double calculatedPi = 1.0;
+double calculatedPi = 1.0; //Global variable to hold grand total for Pi
 
 int main(){
     int i = 0;
     /* Thread creation */
     for(i = 0; i < 8; i++){
         pthread_create(&threads[i], NULL, myFunc, (void *) i);
-        pthread_join(&myThread, NULL);
+        void *myFunc(void *thread);
+        for(int i =0; i < 8; i++){
+            pthread_join(&threads, NULL);
+        }
+
     }
 }
 
@@ -23,16 +27,19 @@ void *myFunc(void *thread){
     pthread_mutex_lock(&lock);
     // Loop 25,000 times
     // Every 10 times, update the global variable
+    for (int i = 0; i < 8; i++) {
+        pthread_join(threads[i], NULL);
+    }
     calculatedPi *= 0; // Wait.. We need to protect the global variable
 }
 
 // Pi calculation formula
 // Source: https://github.com/aureleoules/Pi-WallisProduct/blob/master/main.cpp
 
-double getPI(int decimals, int precision) {
+double getPI() {
     double p, pi = 2.0;
-    for(int i = 2; i <= PRECISION; i += 2) {
+    for(int i = 2; i <= 25000; i += 2) {
         pi = pi * ((p=i)/(i-1))*(p/(i+1));
     }
-    printf("%.*f", decimals, pi);
+    return pi;
 }
